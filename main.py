7c44173fe1db1a31b58a3023fc821c06741924a6
pafnootiy <ksander-86@mail.ru>
2022-11-23 08:28:30 +0400

    fix bugs with timestamp

--- main.py
+++ main.py
@@ -10,45 +10,44 @@ from dotenv import load_dotenv
 
 def get_long_pooling_response(headers):
     long_pooling_url = "https://dvmn.org/api/long_polling/"
-    paylaod={}
+    payload = {}
+    while True:
+        try:
+            response = requests.get(long_pooling_url,
+                                    headers=headers, timeout=200,params=payload)
+            response.raise_for_status()
+            lesson_review = response.json()
 
-    try:
-        response = requests.get(long_pooling_url,
-                                headers=headers, timeout=200,params=paylaod)
-        response.raise_for_status()
-        lesson_review = response.json()
+            if lesson_review["status"] == "found":
+                return lesson_review
 
-        if lesson_review["status"] == "timeout":
-            payload = {
-                'last_attempt_timestamp': lesson_review[
-                    "timestamp_to_request"]
-            }
-        else:
+            elif lesson_review["status"] == "timeout":
+                payload = {
+                    'timestamp': lesson_review[
+                        "timestamp_to_request"]
+                }
+
+        except (requests.exceptions.ReadTimeout,requests.exceptions.HTTPError):
             pass
-    except requests.exceptions.ReadTimeout:
-        pass
-    except requests.exceptions.ConnectionError:
-        print("Ошибка соединения", file=sys.stderr)
-        time.sleep(15)
+        except requests.exceptions.ConnectionError:
+            print("Ошибка соединения", file=sys.stderr)
+            time.sleep(15)
 
-    return lesson_review
 
 def send_message_from_bot(long_pooling_result, user_chat_id, tg_token):
     bot = telegram.Bot(token=tg_token)
 
-    try:
-        if long_pooling_result["new_attempts"][0]["is_negative"]:
-            bot.send_message(
-                text=f'''Ваша работа к уроку {long_pooling_result["new_attempts"][0]["lesson_title"]
-                } - проверенна.Необходимо внести правки и отправить на повторную проверку.Ссылка на урок {
-                long_pooling_result["new_attempts"][0]["lesson_url"]}''',
-                chat_id=user_chat_id)
-        else:
-            bot.send_message(
-                text='''Работа к уроку {response_up["new_attempts"][0]["lesson_title"]} - принята!Приступайте к следующему уроку.''',
-                chat_id=user_chat_id)
-    except:
-        pass
+
+    if long_pooling_result["new_attempts"][0]["is_negative"]:
+        bot.send_message(
+            text=f'''Ваша работа к уроку {long_pooling_result["new_attempts"][0]["lesson_title"]
+            } - проверенна.Необходимо внести правки и отправить на повторную проверку.Ссылка на урок {
+            long_pooling_result["new_attempts"][0]["lesson_url"]}''',
+            chat_id=user_chat_id)
+    else:
+        bot.send_message(
+            text='''Работа к уроку {response_up["new_attempts"][0]["lesson_title"]} - принята!Приступайте к следующему уроку.''',
+            chat_id=user_chat_id)
 
 
 def main():
